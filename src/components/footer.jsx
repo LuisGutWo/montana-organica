b3fe7dbc22187677e08ac37547d4a6904fcdde61
LuisGutWo <agutierrezwong@gmail.com>
2023-10-13 16:17:05 -0400

    fix (footer): load image issue

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
+import shape1 from "../assets/images/footer-v1-shape1.png";
+import shape2 from "../assets/images/footer-v1-shape2.png";
+import shape3 from "../assets/images/footer-v1-shape3.png";
 
 function AppFooter() {
   const [showTopBtn, setShowTopBtn] = useState(false);
@@ -27,17 +30,16 @@ function AppFooter() {
         <div
           className="footer-one__bg"
           style={{
-            backgroundImage:
-              "url(src/assets/images/shapes/footer-v1-shape3.png)",
+            backgroundImage: { shape3 },
           }}
         ></div>
         <div className="shape1 float-bob-y">
-          <img src={"src/assets/images/shapes/footer-v1-shape1.png"} alt="#" />
+          <img src={shape1} alt="#" />
         </div>
         <div className="shape2 float-bob-y">
-          <img src={"src/assets/images/shapes/footer-v1-shape2.png"} alt="#" />
+          <img src={shape2} alt="#" />
         </div>
-        {/* <div className="socials">
+        <div className="socials">
           <ul>
             <li>
               <a href="https://www.facebook.com">
@@ -55,15 +57,15 @@ function AppFooter() {
               </a>
             </li>
           </ul>
-        </div> */}
+        </div>
         {showTopBtn && <div className="go-top" onClick={goTop}></div>}
       </section>
 
-      {/* <section fluid className="copyright-container">
+      <section fluid className="copyright-container">
         <div className="copyright">
           &copy; 2023 LAW-webmedia. All Right Reserved.
         </div>
-      </section> */}
+      </section>
     </>
   );
 }
